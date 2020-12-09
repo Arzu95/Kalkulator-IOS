96dfe28538a3118d758c7dced6002f067b7b780f
Muhammad Arzu <68320293+Arzu95@users.noreply.github.com>
2020-12-09 09:09:52 +0700

    Add files via upload

---
+++ Calculator App/Calculator App/ViewController.swift
@@ -0,0 +1,65 @@
+//
+//  ViewController.swift
+//  Calculator App
+//
+//  Created by Apple on 11/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var inputPanjang: UITextField!
+    @IBOutlet weak var inputLebar: UITextField!
+    @IBOutlet weak var hasil: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    func hitung(aritmatika: String){
+        if inputPanjang.text == "" || inputLebar.text == ""{
+            hasil.text = "Input Tidak Boleh Kosong"
+        }
+        else{
+            let nilaiPanjang = Double(inputPanjang.text!)
+            let nilaiLebar = Double(inputLebar.text!)
+            
+            var jumlah: Double?
+            
+            switch aritmatika {
+            case "+":
+                jumlah = nilaiPanjang! + nilaiLebar!
+            case "-":
+                jumlah = nilaiPanjang! - nilaiLebar!
+            case "*":
+                jumlah = nilaiPanjang! * nilaiLebar!
+            case "/":
+                jumlah = nilaiPanjang! / nilaiLebar!
+            default:
+                break
+            }
+            
+            hasil.text = String(jumlah! )
+        }
+    }
+    
+    @IBAction func btnTambah(_ sender: UIButton) {
+        hitung(aritmatika: "+")
+    }
+    
+    @IBAction func btnKuning(_ sender: UIButton) {
+        hitung(aritmatika: "-")
+    }
+    
+    @IBAction func btnKali(_ sender: UIButton) {
+        hitung(aritmatika: "*")
+    }
+    
+    @IBAction func btnBagi(_ sender: UIButton) {
+        hitung(aritmatika: "/")
+    }
+    
+}
+
